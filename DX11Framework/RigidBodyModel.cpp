de05d8dc79bed9590efb6c20ce976f4a385a7e5f
Cai Mills <CaiMills111@outlook.com>
2025-01-21 23:12:08 +0000

    InertiaTensor Calculations (I think)

--- DX11Framework/RigidBodyModel.cpp
+++ DX11Framework/RigidBodyModel.cpp
@@ -9,5 +9,7 @@ void RigidBodyModel::AddRelativeForce(Vector3 force, Vector3 point)
 	crossProduct.y = (force.z * point.x) - (force.x * point.z);
 	crossProduct.z = (force.x * point.y) - (force.y * point.x);
 
+	//XMStoreFloat3(&cross, XMVector3Cross(XMVectorSet(force.x, force.y, force.z, 0), XMVectorSet(point.x, point.y, point.z, 0)));
+
 	_torque = crossProduct;
 }
--- DX11Framework/RigidBodyModel.h
+++ DX11Framework/RigidBodyModel.h
@@ -14,9 +14,33 @@ public:
         //Sets the Inertia Tensor to Identity Matrix by default
         XMMATRIX identity = XMMatrixIdentity();
         XMStoreFloat3x3(&_inertiaTensor, identity);
+
+        //Circle
+        //_inertiaTensor._11 = (2.0f / 5.0f) * _mass * (GetCollider()->GetRadius() * GetCollider()->GetRadius());
+        //_inertiaTensor._12 = 0;
+        //_inertiaTensor._13 = 0;
+
+        //_inertiaTensor._21 = 0;
+        //_inertiaTensor._22 = (2.0f / 5.0f) * _mass * (GetCollider()->GetRadius() * GetCollider()->GetRadius());
+        //_inertiaTensor._23 = 0;
+
+        //_inertiaTensor._31 = 0;
+        //_inertiaTensor._32 = 0;
+        //_inertiaTensor._33 = (2.0f / 5.0f) * _mass * (GetCollider()->GetRadius() * GetCollider()->GetRadius());
+
+        //Box 
+        _inertiaTensor._11 = (1.0f / 12.0f) * _mass * ((_transform->GetScale().y/2) * (_transform->GetScale().y/2) + (_transform->GetScale().z/2) * (_transform->GetScale().z/2));
+        _inertiaTensor._12 = 0;
+        _inertiaTensor._13 = 0;
+
+        _inertiaTensor._21 = 0;
+        _inertiaTensor._22 = (1.0f / 12.0f) * _mass * ((_transform->GetScale().x / 2) * (_transform->GetScale().x / 2) + (_transform->GetScale().z / 2) * (_transform->GetScale().z / 2));
+        _inertiaTensor._23 = 0;
+
+        _inertiaTensor._31 = 0;
+        _inertiaTensor._32 = 0;
+        _inertiaTensor._33 = (1.0f / 12.0f) * _mass * ((_transform->GetScale().x / 2) * (_transform->GetScale().x / 2) + (_transform->GetScale().y / 2) * (_transform->GetScale().y / 2));
     };
 
     void AddRelativeForce(Vector3 force, Vector3 point) override;
-
-    //XMStoreFloat3(torque, XMVector3Cross(XMVectorSet(force.x, force.y, force.z, 0), XMVectorSet(point.x, point.y, point.z, 0)))
 };
