b19bf25bc8957bfffd3d4eef83034d8f8a1984ed
Cai Mills <CaiMills111@outlook.com>
2025-01-21 21:09:13 +0000

    Converted some old code

--- DX11Framework/DX11Framework.cpp
+++ DX11Framework/DX11Framework.cpp
@@ -399,7 +399,7 @@ HRESULT DX11Framework::InitRunTimeData()
     _floor->SetAppearance(appearance);
     _floor->GetTransform()->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
     _floor->GetTransform()->SetScale(Vector3(15.0f, 15.0f, 15.0f));
-    _floor->GetTransform()->SetRotation(Vector3(XMConvertToRadians(90.0f), 0.0f, 0.0f));
+    _floor->GetTransform()->SetRotation(Vector3(XMConvertToRadians(40.0f), 0.0f, 0.0f)); //It wont work without the XMConvertToRadians
 
     _gameObjects.push_back(_floor);
 
--- DX11Framework/Transform.cpp
+++ DX11Framework/Transform.cpp
@@ -15,27 +15,37 @@ Transform::~Transform()
 
 void Transform::Update(float deltaTime)
 {
-	if (_continuousRotation)
-	{
-		static float rotationAngle = 0.0f; // Initialize once
-		rotationAngle += deltaTime * 2; // Adjust rotation speed as needed
+	//if (_continuousRotation)
+	//{
+	//	//Constant Rotation
+	//	static float rotationAngle = 0.0f;
+	//	rotationAngle += deltaTime;
 
-		// Calculate world matrix
-		XMMATRIX scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
-		XMMATRIX rotation = XMMatrixRotationX(rotationAngle) * XMMatrixRotationY(rotationAngle);
-		XMMATRIX position = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
+	//	// Calculate world matrix
+	//	XMMATRIX scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
+	//	XMVECTOR rotation = XMVectorSet(rotationAngle, GetRotation().y, GetRotation().z, 1);
+	//	XMMATRIX position = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
 
-		XMStoreFloat4x4(&_world, XMMatrixIdentity() * scale * rotation * position);
-	}
-	else
-	{
-		// Calculate world matrix
-		XMMATRIX scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
-		XMMATRIX rotation = XMMatrixRotationX(GetRotation().x) * XMMatrixRotationY(GetRotation().y) * XMMatrixRotationZ(GetRotation().z);
-		XMMATRIX position = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
+	//	XMMATRIX orientation = XMMatrixRotationQuaternion(rotation);
 
-		XMStoreFloat4x4(&_world, XMMatrixIdentity() * scale * rotation * position);
-	}
+	//	XMStoreFloat4x4(&_world, XMMatrixIdentity() * scale * orientation * position);
+	//}
+	//else
+	//{
+	//	// Calculate world matrix
+	//	XMMATRIX scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
+	//	XMMATRIX orientation = XMMatrixRotationQuaternion(XMVectorSet(GetRotation().x, GetRotation().y, GetRotation().z, 1));
+	//	XMMATRIX position = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
+
+	//	XMStoreFloat4x4(&_world, XMMatrixIdentity() * scale * orientation * position);
+	//}
+
+	// Calculate world matrix
+	XMMATRIX scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
+	XMMATRIX orientation = XMMatrixRotationQuaternion(XMVectorSet(GetRotation().x, GetRotation().y, GetRotation().z, 1));
+	XMMATRIX position = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
+
+	XMStoreFloat4x4(&_world, XMMatrixIdentity() * scale * orientation * position);
 }
 
 void Transform::Move(Vector3 direction)
