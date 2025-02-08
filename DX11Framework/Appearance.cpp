56ca2ad421ad2867d42d51635104a637b26e7907
Cai Mills <CaiMills111@outlook.com>
2025-02-08 20:01:04 +0000

    Update Appearance.cpp

--- DX11Framework/Appearance.cpp
+++ DX11Framework/Appearance.cpp
@@ -16,9 +16,6 @@ void Appearance::Draw()
 {
 	if (_mesh->GetMeshData()->VertexBuffer != nullptr && _mesh->GetMeshData()->IndexBuffer != nullptr)
 	{
-		// Set texture
-		_immediateContext->PSSetShaderResources(0, 1, &_texture);
-
 		// Set vertex and index buffers
 		_immediateContext->IASetVertexBuffers(0, 1, &_mesh->GetMeshData()->VertexBuffer, &_mesh->GetMeshData()->VBStride, &_mesh->GetMeshData()->VBOffset);
 		_immediateContext->IASetIndexBuffer(_mesh->GetMeshData()->IndexBuffer, DXGI_FORMAT_R16_UINT, 0);
