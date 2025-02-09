59e8df1336e0dee0fae9e3526dcf5bdb67a170ad
Cai Mills <CaiMills111@outlook.com>
2025-02-09 02:06:14 +0000

    box collision FINALLY works

--- DX11Framework/BoxCollider.cpp
+++ DX11Framework/BoxCollider.cpp
@@ -4,18 +4,16 @@
 
 bool BoxCollider::CollidesWith(BoxCollider& other, CollisionManifold& out)
 {
-    //// AABB vs AABB Collision (OUTDATED)
-    //if (_appearance->GetMesh()->GetMin().x <= other.GetAppearance()->GetMesh()->GetMax().x && _appearance->GetMesh()->GetMax().x >= other.GetAppearance()->GetMesh()->GetMin().x &&
-    //    _appearance->GetMesh()->GetMin().y <= other.GetAppearance()->GetMesh()->GetMax().y && _appearance->GetMesh()->GetMax().y >= other.GetAppearance()->GetMesh()->GetMin().y &&
-    //    _appearance->GetMesh()->GetMin().z <= other.GetAppearance()->GetMesh()->GetMax().z && _appearance->GetMesh()->GetMax().z >= other.GetAppearance()->GetMesh()->GetMin().z)
-    //{
-    //    DebugPrintF("AABB vs AABB Collision\n");
-    //    return true;
-    //}
-    //else
-    //{
-    //    return false;
-    //}
+    Vector3 distance = GetPosition() - other.GetPosition();
+
+    Vector3 objAExtents = GetAppearance()->GetMesh()->GetExtents();
+    Vector3 objBExtents = other.GetAppearance()->GetMesh()->GetExtents();
+    Vector3 halfExtents = objAExtents / 2 + objBExtents / 2;
+
+    if (distance.x <= halfExtents.x && distance.y <= halfExtents.y && distance.z <= halfExtents.z)
+    {
+        return true;
+    }
 
     return false;
 }
